c3bce9c4dc21602d15e160c4b5cbf021cfd710cf
Hal Clark <hallamjsclark@gmail.com>
2023-02-06 22:06:28 +0000

    Changed all modid references to 'redstonetools'

--- src/main/java/net/redstonetools/example/mixin/ExampleMixin.java
+++ src/main/java/net/redstonetools/example/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.redstonetools.example.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.redstonetools.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
