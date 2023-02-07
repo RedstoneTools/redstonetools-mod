5a7165a38b341341993b938fefd33c730a5cdc00
Orbyfied <officialglitchout@gmail.com>
2023-02-07 22:38:04 +0100

    (?) core: improve design i guess

--- src/main/java/com/domain/redstonetools/RedstoneToolsClient.java
+++ src/main/java/com/domain/redstonetools/RedstoneToolsClient.java
@@ -1,37 +1,90 @@
 package com.domain.redstonetools;
 
-import com.domain.redstonetools.features.commands.CommandFeature;
+import com.domain.redstonetools.features.Feature;
 import com.domain.redstonetools.features.commands.quicktp.QuickTpFeature;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class RedstoneToolsClient implements ClientModInitializer {
+public final class RedstoneToolsClient implements ClientModInitializer {
+
     public static final String MOD_ID = "redstonetools";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    private static final List<CommandFeature> FEATURES = List.of(
-        new QuickTpFeature()
+    ////////////////////////////////////////////
+
+    /**
+     * Initial features to be initialized.
+     *
+     * The classes are stored instead of the
+     * objects to avoid instantiating them before
+     * any other services provided by the mod are
+     * initialized.
+     */
+    private static final List<Class<? extends Feature>> FEATURES = List.of(
+            QuickTpFeature.class
     );
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Initializing Redstone Tools");
 
-        registerFeatures();
+        registerInitialFeatures();
     }
 
-    private void registerFeatures() {
-        for (var feature : FEATURES) {
-            registerFeature(feature);
+    /*
+        Feature Registry
+     */
+
+    private final Map<String, Feature> featureMap = new HashMap<>();
+
+    public Feature getFeature(String str) {
+        return featureMap.get(str);
+    }
+
+    public boolean registerFeature(Feature feature) {
+        try {
+            feature.register();
+            featureMap.put(feature.getName(), feature);
+            return true;
+        } catch (Exception e) {
+            LOGGER.error("Error while initializing feature '" + feature.getName() + "'");
+            e.printStackTrace();
+
+            return false;
         }
     }
 
-    private void registerFeature(CommandFeature feature) {
-        CommandRegistrationCallback.EVENT.register(feature::registerCommand);
+    public Feature createAndRegisterFeature(Class<? extends Feature> klass) {
+        try {
+            Constructor<?> constructor = klass.getDeclaredConstructor();
+            Feature instance = (Feature) constructor.newInstance();
+
+            if (!registerFeature(instance)) {
+                return null;
+            }
+
+            return instance;
+        } catch (Exception e) {
+            LOGGER.error("Failed to instantiate feature " + klass.getName());
+            e.printStackTrace();
+
+            return null;
+        }
     }
+
+    // registers all initial features
+    // from the constant list
+    private void registerInitialFeatures() {
+        for (var feature : FEATURES) {
+            createAndRegisterFeature(feature);
+        }
+    }
+
 }
---
+++ src/main/java/com/domain/redstonetools/features/FeatureComponent.java
@@ -0,0 +1,11 @@
+package com.domain.redstonetools.features;
+
+/**
+ * A component of a feature which can
+ * be independently initialized.
+ */
+public interface FeatureComponent {
+
+    void register();
+
+}
