e89669ed2b7f6601a45ac66cbc970563b71b7e0d
rugmj <rugmj@rugmj.dev>
2023-02-14 17:11:23 +0000

    removed unneeded type

--- src/main/java/com/domain/redstonetools/features/commands/baseconvert/BaseConvertOptions.java
+++ src/main/java/com/domain/redstonetools/features/commands/baseconvert/BaseConvertOptions.java
@@ -8,5 +8,5 @@ import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
 public class BaseConvertOptions extends Options {
     public final Argument<Integer> fromBase = new Argument<>("fromBase", integer(2, 36));
     public final Argument<Integer> number = new Argument<>("number", integer());
-    public final Argument<Integer> toBase = new Argument<Integer>("toBase", integer(2, 36));
+    public final Argument<Integer> toBase = new Argument<>("toBase", integer(2, 36));
 }
