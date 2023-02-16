8886029df9444626097f0ae45d08891f445356fb
Matthias Wijnsma <matthiasx95@gmail.com>
2023-02-16 19:55:36 +0100

    Refactor

--- src/main/java/com/domain/redstonetools/RedstoneToolsGameRules.java
+++ src/main/java/com/domain/redstonetools/RedstoneToolsGameRules.java
@@ -7,14 +7,12 @@ import net.minecraft.world.GameRules;
 import static com.domain.redstonetools.RedstoneToolsClient.LOGGER;
 
 public class RedstoneToolsGameRules {
-
     private RedstoneToolsGameRules() {
     }
 
     public static GameRules.Key<GameRules.BooleanRule> DO_CONTAINER_DROPS;
 
     public static void register() {
-        DO_CONTAINER_DROPS = GameRuleRegistry.register("doContainerDrops",GameRules.Category.DROPS,GameRuleFactory.createBooleanRule(true));
+        DO_CONTAINER_DROPS = GameRuleRegistry.register("doContainerDrops", GameRules.Category.DROPS, GameRuleFactory.createBooleanRule(true));
     }
-
 }
