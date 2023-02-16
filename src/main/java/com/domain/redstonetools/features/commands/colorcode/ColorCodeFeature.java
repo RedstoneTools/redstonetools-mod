4d860a12bbf4274a619818d280383d710e04f20b
Orbyfied <officialglitchout@gmail.com>
2023-02-16 23:08:22 +0100

    fix /colorcode: separate check into mask

--- src/main/java/com/domain/redstonetools/features/commands/colorcode/ColorCodeFeature.java
+++ src/main/java/com/domain/redstonetools/features/commands/colorcode/ColorCodeFeature.java
@@ -3,14 +3,14 @@ package com.domain.redstonetools.features.commands.colorcode;
 import com.domain.redstonetools.features.Feature;
 import com.domain.redstonetools.features.commands.CommandFeature;
 import com.domain.redstonetools.utils.CharTreeNode;
-import com.domain.redstonetools.utils.WorldEditUtils;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.EditSessionBuilder;
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.fabric.FabricAdapter;
 import com.sk89q.worldedit.fabric.FabricPlayer;
+import com.sk89q.worldedit.function.mask.Mask;
+import com.sk89q.worldedit.function.mask.Mask2D;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.function.pattern.Pattern;
 import com.sk89q.worldedit.math.BlockVector3;
@@ -19,16 +19,12 @@ import com.sk89q.worldedit.world.World;
 import com.sk89q.worldedit.world.block.BaseBlock;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockType;
-import net.minecraft.block.Block;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -46,27 +42,48 @@ public class ColorCodeFeature extends CommandFeature<ColorCodeFeatureOptions> {
     }
 
     // checks if the block at the position
-    // is a target and if so updates it
-    // returns if it replaced the block
-    private BaseBlock checkAndUpdateBlock(World world,
-                                          BlockVector3 pos,
-                                          String color,
-                                          boolean onlyWhite) {
+    // is a target for the transformation
+    private boolean checkBlock(World world,
+                               BlockVector3 pos,
+                               boolean onlyWhite) {
         BlockState state = world.getBlock(pos);
         if (state == null)
-            return null;
+            return false;
 
         // check if it is a target
         String blockId = state.getBlockType().getId();
         int colorlessBlockIdIndex;
         if ((colorlessBlockIdIndex = MATCH_TARGET_PATH.findEndMatch(blockId)) == -1)
-            return null;
+            return false;
         String colorlessBlockId = blockId.substring(colorlessBlockIdIndex);
         if (onlyWhite && !blockId.substring(0, colorlessBlockIdIndex).equals("white"))
-            return null;
+            return false;
 
+        return true;
+    }
+
+    // changes the block at the given position
+    // to the correct color if it is a target
+    // all non targets should have been excluded
+    // by the mask, but just in case
+    private BaseBlock changeBlock(World world,
+                                  BlockVector3 pos,
+                                  String color) {
+        BlockState state = world.getBlock(pos);
+        BlockType oldType = state.getBlockType();
+        String blockId = oldType.getId();
+
+        // get colorless id
+        int colorlessBlockIdIndex = MATCH_TARGET_PATH.findEndMatch(blockId);
+        if (colorlessBlockIdIndex == -1)
+            return state.toBaseBlock();
+        String colorlessBlockId = blockId.substring(colorlessBlockIdIndex);
+
+        // get colored block
         String coloredId = "minecraft:" + color + colorlessBlockId;
         BlockType blockType = BlockType.REGISTRY.get(coloredId);
+        if (blockType == null)
+            return state.toBaseBlock();
 
         return blockType.getDefaultState().toBaseBlock();
     }
@@ -103,16 +120,30 @@ public class ColorCodeFeature extends CommandFeature<ColorCodeFeatureOptions> {
         final World world = FabricAdapter.adapt(player.getWorld());
         try (EditSession session = worldEdit.newEditSession(FabricAdapter.adapt(player.getWorld()))) {
             AtomicInteger counter = new AtomicInteger();
+
+            // create mask
+            session.setMask(new Mask() {
+                @Override
+                public boolean test(BlockVector3 vector) {
+                    return checkBlock(world, vector, onlyWhite);
+                }
+
+                @Nullable
+                @Override
+                public Mask2D toMask2D() { return null; }
+            });
+
+            // create pattern and execute block set
             counter.set(session.setBlocks(selection, new Pattern() {
                 @Override
                 public BaseBlock applyBlock(BlockVector3 position) {
-                    return checkAndUpdateBlock(world, position, color, onlyWhite);
+                    return changeBlock(world, position, color);
                 }
             }));
 
             Operations.complete(session.commit());
 
-            // remember to allow undo
+            // call remember to allow undo
             playerSession.remember(session);
 
             source.sendFeedback(Text.of("Successfully changed " + counter.get() + " blocks to color " + color)
