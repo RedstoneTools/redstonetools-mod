2860c00525dfccb1af34a699fb436886d198ffc9
rugmj <rugmj@rugmj.dev>
2023-02-16 20:33:41 +0000

    Catched number format exception

--- src/main/java/com/domain/redstonetools/features/commands/baseconvert/BaseConvertFeature.java
+++ src/main/java/com/domain/redstonetools/features/commands/baseconvert/BaseConvertFeature.java
@@ -3,7 +3,10 @@ package com.domain.redstonetools.features.commands.baseconvert;
 import com.domain.redstonetools.features.Feature;
 import com.domain.redstonetools.features.commands.CommandFeature;
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.exceptions.CommandExceptionType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
@@ -14,13 +17,18 @@ public class BaseConvertFeature extends CommandFeature<BaseConvertOptions> {
     @Override
     protected int execute(ServerCommandSource source, BaseConvertOptions options) throws CommandSyntaxException {
 
-        String result = Integer.toString(
-                Integer.parseInt(options.number.getValue(), options.fromBase.getValue()),
-                options.toBase.getValue());
+        try {
+            String result = Integer.toString(
+                    Integer.parseInt(options.number.getValue(), options.fromBase.getValue()),
+                    options.toBase.getValue());
+            source.sendFeedback(Text.of(result), false);
+            return Command.SINGLE_SUCCESS;
+        } catch (NumberFormatException e) {
+            // TODO: handle exception
+            LiteralMessage message = new LiteralMessage("The input number does not match the input base");
+            throw new CommandSyntaxException(new SimpleCommandExceptionType(message), message);
+        }
 
-        source.sendFeedback(Text.of(result), false);
-
-        return Command.SINGLE_SUCCESS;
     }
 
 }
