4ae2930938a1b077a0d2a240904e597534828453
Matthias Wijnsma <matthiasx95@gmail.com>
2023-02-19 12:44:44 +0100

    Refactor binary block read feature

--- src/main/java/com/domain/redstonetools/features/commands/binaryread/BinaryBlockReadFeature.java
+++ src/main/java/com/domain/redstonetools/features/commands/binaryread/BinaryBlockReadFeature.java
@@ -1,89 +1,70 @@
 package com.domain.redstonetools.features.commands.binaryread;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
-import com.domain.redstonetools.RedstoneToolsClient;
 import com.domain.redstonetools.features.Feature;
 import com.domain.redstonetools.features.commands.CommandFeature;
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.sk89q.worldedit.IncompleteRegionException;
-import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.WorldEdit;
-import com.sk89q.worldedit.entity.Player;
 import com.sk89q.worldedit.fabric.FabricAdapter;
 import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.session.SessionManager;
 import com.sk89q.worldedit.util.formatting.text.TextComponent;
-import com.sk89q.worldedit.world.World;
-
-import net.minecraft.block.BlockState;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
 @Feature(name = "/read")
 public class BinaryBlockReadFeature extends CommandFeature<BinaryBlockReadOptions> {
-
     @Override
     protected int execute(ServerCommandSource source, BinaryBlockReadOptions options) throws CommandSyntaxException {
+        var actor = FabricAdapter.adaptPlayer(source.getPlayer());
+        var localSession = WorldEdit.getInstance()
+                .getSessionManager()
+                .get(actor);
+        var selectionWorld = localSession.getSelectionWorld();
 
-        Integer SPACING = options.spacing.getValue();
-
-        Player actor = FabricAdapter.adaptPlayer(source.getPlayer());
-        SessionManager manager = WorldEdit.getInstance().getSessionManager();
-        LocalSession localSession = manager.get(actor);
-
-        Region region;
-        World selectionWorld = localSession.getSelectionWorld();
-
+        Region selection;
         try {
-            if (selectionWorld == null)
+            if (selectionWorld == null)  {
                 throw new IncompleteRegionException();
-            region = localSession.getSelection(selectionWorld);
-        } catch (IncompleteRegionException ex) {
-            actor.printError(TextComponent.of("Please make a selection with worldedit first."));
-            return 0;
-        }
-
-        CuboidRegion regionBox = region.getBoundingBox();
-        BlockVector3 pos1 = regionBox.getPos1();
-        BlockVector3 pos2 = regionBox.getPos2();
-        BlockVector3 direction = pos2.subtract(pos1).normalize();
-        BlockVector3 spacer = direction.multiply(SPACING);
-
-        if ((direction.getX() + direction.getBlockY() + direction.getBlockZ()) > 1) {
-            source.sendError(Text.of("The selection must have 2 axis the same"));
-            return 0;
-        }
-
-        StringBuilder bits = new StringBuilder();
-
-        for (BlockVector3 point = pos1; regionBox.contains(point); point = point.add(spacer)) {
-            BlockState pointBlock = FabricAdapter.adapt(selectionWorld.getBlock(point));
-
-            if (options.onBlock.getValue().getBlockState() == pointBlock) {
-                bits.append(1);
-            } else {
-                bits.append(0);
             }
 
+            selection = localSession.getSelection(selectionWorld);
+        } catch (IncompleteRegionException ex) {
+            actor.printError(TextComponent.of("Please make a selection with worldedit first."));
+
+            return -1;
         }
 
-        if (!options.reverseBits.getValue()) {
+        var boundingBox = selection.getBoundingBox();
+        var pos1 = boundingBox.getPos1();
+        var pos2 = boundingBox.getPos2();
+        var direction = pos2.subtract(pos1).normalize();
+        var spacing = direction.multiply(options.spacing.getValue());
+
+        if (direction.getX() + direction.getBlockY() + direction.getBlockZ() > 1) {
+            actor.printError(TextComponent.of("The selection must have 2 axis the same"));
+
+            return -1;
+        }
+
+        var onBlockState = options.onBlock.getValue().getBlockState();
+
+        var bits = new StringBuilder();
+        for (BlockVector3 point = pos2; boundingBox.contains(point); point = point.subtract(spacing)) {
+            var pointBlockState = FabricAdapter.adapt(selectionWorld.getBlock(point));
+
+            bits.append(pointBlockState.equals(onBlockState) ? 1 : 0);
+        }
+
+        if (options.reverseBits.getValue()) {
             bits.reverse();
         }
 
-        String result = Integer.toString(
-                Integer.parseInt(bits.toString(), 2),
-                options.toBase.getValue());
+        var output = Integer.toString(Integer.parseInt(bits.toString(), 2), options.toBase.getValue());
+        source.sendFeedback(Text.of(output), false);
 
-        source.sendFeedback(Text.of(result), false);
-
-        return 1;
+        return Command.SINGLE_SUCCESS;
     }
 
 }
