34ccaef9edfa73868c60f75fda83c5be7f87d164
rugmj <rugmj@rugmj.dev>
2023-02-27 17:10:04 +0000

    revised redstoner

--- src/main/java/com/domain/redstonetools/features/commands/RedstonerFeature.java
+++ src/main/java/com/domain/redstonetools/features/commands/RedstonerFeature.java
@@ -5,7 +5,10 @@ import com.domain.redstonetools.features.Feature;
 import com.mojang.brigadier.Command;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.GameRules.BooleanRule;
+import net.minecraft.world.GameRules.Key;
 
 @Feature(name = "Redstoner", description = "Sets the game rules to be more redstone friendly.", command = "redstoner")
 public class RedstonerFeature extends CommandFeature {
@@ -14,15 +17,20 @@ public class RedstonerFeature extends CommandFeature {
         GameRules rules = source.getWorld().getGameRules();
         MinecraftServer server = source.getServer();
 
-        rules.get(GameRules.DO_TILE_DROPS).set(false, server);
-        rules.get(GameRules.DO_TRADER_SPAWNING).set(false, server);
-        rules.get(GameRules.DO_WEATHER_CYCLE).set(false, server);
-        rules.get(GameRules.DO_DAYLIGHT_CYCLE).set(false, server);
-        rules.get(GameRules.DO_MOB_SPAWNING).set(false, server);
-        rules.get(RedstoneToolsGameRules.DO_CONTAINER_DROPS).set(false, server);
+        setRule(GameRules.DO_TILE_DROPS, false, rules, server, source);
+        setRule(GameRules.DO_TRADER_SPAWNING, false, rules, server, source);
+        setRule(GameRules.DO_WEATHER_CYCLE, false, rules, server, source);
+        setRule(GameRules.DO_DAYLIGHT_CYCLE, false, rules, server, source);
+        setRule(GameRules.DO_MOB_SPAWNING, false, rules, server, source);
+        setRule(RedstoneToolsGameRules.DO_CONTAINER_DROPS, false, rules, server, source);
 
         source.getWorld().setTimeOfDay(6000); // Noon
 
         return Command.SINGLE_SUCCESS;
+    }
+
+    private void setRule(Key<BooleanRule> rule, Boolean value, GameRules rules, MinecraftServer server, ServerCommandSource source) {
+        rules.get(rule).set(value, server);
+        source.sendFeedback(Text.of(String.format("Gamerule %s is now set to: %s", rule.getName(), value.toString())), true);
     }
 }
