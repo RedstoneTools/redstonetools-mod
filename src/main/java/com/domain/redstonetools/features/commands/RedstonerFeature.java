195e87159c06275bd730d90ae33f9bdb0ef4f6fe
rugmj <rugmj@rugmj.dev>
2023-02-28 19:52:07 +0000

    Revert "Refactored RedstonerFeature"
    This reverts commit 823b5fc742f86b0d05cd11b80a2d2cac8e5b6bbb.

--- src/main/java/com/domain/redstonetools/features/commands/RedstonerFeature.java
+++ src/main/java/com/domain/redstonetools/features/commands/RedstonerFeature.java
@@ -1,36 +1,27 @@
 package com.domain.redstonetools.features.commands;
 
-import com.domain.redstonetools.RedstoneToolsGameRules;
 import com.domain.redstonetools.features.Feature;
+import com.domain.redstonetools.utils.CommandSourceUtils;
 import com.mojang.brigadier.Command;
-import net.minecraft.server.MinecraftServer;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.Text;
-import net.minecraft.world.GameRules;
-import net.minecraft.world.GameRules.BooleanRule;
-import net.minecraft.world.GameRules.Key;
+
+import java.util.List;
 
 @Feature(name = "Redstoner", description = "Sets the game rules to be more redstone friendly.", command = "redstoner")
 public class RedstonerFeature extends CommandFeature {
     @Override
-    protected int execute(ServerCommandSource source) {
-        GameRules rules = source.getWorld().getGameRules();
-        MinecraftServer server = source.getServer();
-
-        setRule(GameRules.DO_TILE_DROPS, false, rules, server, source);
-        setRule(GameRules.DO_TRADER_SPAWNING, false, rules, server, source);
-        setRule(GameRules.DO_WEATHER_CYCLE, false, rules, server, source);
-        setRule(GameRules.DO_DAYLIGHT_CYCLE, false, rules, server, source);
-        setRule(GameRules.DO_MOB_SPAWNING, false, rules, server, source);
-        setRule(RedstoneToolsGameRules.DO_CONTAINER_DROPS, false, rules, server, source);
-
-        source.getWorld().setTimeOfDay(6000); // Noon
+    protected int execute(ServerCommandSource source) throws CommandSyntaxException {
+        List.of(
+                "gamerule doTileDrops false",
+                "gamerule doTraderSpawning false",
+                "gamerule doWeatherCycle false",
+                "gamerule doDaylightCycle false",
+                "gamerule doMobSpawning false",
+                "gamerule doContainerDrops false",
+                "time set noon"
+                ).forEach(cmd -> CommandSourceUtils.executeCommand(source, cmd));
 
         return Command.SINGLE_SUCCESS;
-    }
-
-    private void setRule(Key<BooleanRule> rule, Boolean value, GameRules rules, MinecraftServer server, ServerCommandSource source) {
-        rules.get(rule).set(value, server);
-        source.sendFeedback(Text.of(String.format("Gamerule %s is now set to: %s", rule.getName(), value.toString())), true);
     }
 }
