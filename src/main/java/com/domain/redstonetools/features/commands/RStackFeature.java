647ad81e7c17861d65aab8faa1974586ab59ca66
DiamondTaco <imadiamondtaco@gmail.com>
2023-03-07 11:10:50 -0600

    Format

--- src/main/java/com/domain/redstonetools/features/commands/RStackFeature.java
+++ src/main/java/com/domain/redstonetools/features/commands/RStackFeature.java
@@ -2,18 +2,22 @@ package com.domain.redstonetools.features.commands;
 
 import com.domain.redstonetools.features.Feature;
 import com.domain.redstonetools.features.arguments.Argument;
-import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.sk89q.worldedit.IncompleteRegionException;
 import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.fabric.FabricAdapter;
+import com.sk89q.worldedit.function.mask.Mask;
+import com.sk89q.worldedit.function.mask.Mask2D;
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.util.Direction;
 import com.sk89q.worldedit.util.formatting.text.TextComponent;
 import net.minecraft.server.command.ServerCommandSource;
+import org.jetbrains.annotations.Nullable;
 
-import static com.domain.redstonetools.features.arguments.DirectionArgumentType.directionArgument;
-import static com.domain.redstonetools.features.arguments.DirectionArgumentType.firstOrdinal;
+import static com.domain.redstonetools.features.arguments.DirectionArgumentType.*;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
 
 @Feature(name = "rstack", description = "Stacks with custom distance", command = "/rstack")
@@ -30,10 +34,10 @@ public class RStackFeature extends CommandFeature {
             .ofType(integer())
             .withDefault(2);
 
-
     @Override
     protected int execute(ServerCommandSource source) throws CommandSyntaxException {
         var actor = FabricAdapter.adaptPlayer(source.getPlayer());
+
         var localSession = WorldEdit.getInstance()
                 .getSessionManager()
                 .get(actor);
@@ -48,7 +52,6 @@ public class RStackFeature extends CommandFeature {
             selection = localSession.getSelection(selectionWorld);
         } catch (IncompleteRegionException ex) {
             actor.printError(TextComponent.of("Please make a selection with worldedit first."));
-
             return -1;
         }
 
