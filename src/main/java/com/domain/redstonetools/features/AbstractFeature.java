7b088840b0ec769ed610806e881db1c053cfec47
Orbyfied <officialglitchout@gmail.com>
2023-03-08 14:59:49 +0100

    AbstractFeature: add cached feature desc

--- src/main/java/com/domain/redstonetools/features/AbstractFeature.java
+++ src/main/java/com/domain/redstonetools/features/AbstractFeature.java
@@ -5,9 +5,30 @@ import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.server.command.ServerCommandSource;
 
 public abstract class AbstractFeature {
+
+    // cached descriptor for this
+    // feature to avoid regular slow
+    // reflection
+    final Feature featureDesc;
+
+    {
+        featureDesc = getClass().getAnnotation(Feature.class);
+        if (featureDesc == null)
+            throw new IllegalStateException("Supposed feature " + getClass() + " does not" +
+                    " have an @Feature descriptor annotation");
+    }
+
+    public Feature getFeatureDescriptor() {
+        return featureDesc;
+    }
+
+    /**
+     * Register this feature.
+     */
     public void register() {
         CommandRegistrationCallback.EVENT.register(this::registerCommands);
     }
 
     protected abstract void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated);
+
 }
