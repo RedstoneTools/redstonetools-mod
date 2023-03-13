6d9a4b417674ac1114aad1d6094184d5567e14d3
Ninty <ninty0808@gmail.com>
2023-03-13 09:15:19 +0100

    Update ToggleableFeature.java

--- src/main/java/com/domain/redstonetools/features/toggleable/ToggleableFeature.java
+++ src/main/java/com/domain/redstonetools/features/toggleable/ToggleableFeature.java
@@ -2,6 +2,8 @@ package com.domain.redstonetools.features.toggleable;
 
 import com.domain.redstonetools.features.AbstractFeature;
 import com.domain.redstonetools.features.Feature;
+import com.domain.redstonetools.feedback.Feedback;
+import com.domain.redstonetools.feedback.FeedbackSender;
 import com.domain.redstonetools.utils.ReflectionUtils;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
@@ -9,6 +11,8 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
+
+import static com.domain.redstonetools.RedstoneToolsClient.INJECTOR;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public abstract class ToggleableFeature extends AbstractFeature {
@@ -35,12 +39,12 @@ public abstract class ToggleableFeature extends AbstractFeature {
 
     //TODO: these need to be replaced when the sendMessage util gets made.
     protected int onEnable(ServerCommandSource source) throws CommandSyntaxException {
-        source.getPlayer().sendMessage(Text.of(info.name() + " has been enabled."), false);
+        INJECTOR.getInstance(FeedbackSender.class).sendFeedback(source, Feedback.success(info.name() + " has been enabled."));
         return 0;
     }
 
     protected int onDisable(ServerCommandSource source) throws CommandSyntaxException {
-        source.getPlayer().sendMessage(Text.of(info.name() + " has been disabled."), false);
+        INJECTOR.getInstance(FeedbackSender.class).sendFeedback(source, Feedback.success(info.name() + " has been enabled."));
         return 0;
     }
 }
