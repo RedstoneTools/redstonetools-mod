dff6d0ef47549a24439acc89f6f659b6a0370ee2
Matthias Wijnsma <matthiasx95@gmail.com>
2023-04-12 16:26:55 +0200

    Swap fromBase and number argument in base convert feature

--- src/main/java/tools/redstone/redstonetools/features/commands/BaseConvertFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/commands/BaseConvertFeature.java
@@ -12,10 +12,10 @@ import java.math.BigInteger;
 
 @Feature(name = "Base Convert", description = "Converts a number from one base to another.", command = "base")
 public class BaseConvertFeature extends CommandFeature {
-    public static final Argument<Integer> fromBase = Argument
-            .ofType(integer(2, 36));
     public static final Argument<String> number = Argument
             .ofType(word());
+    public static final Argument<Integer> fromBase = Argument
+            .ofType(integer(2, 36));
     public static final Argument<Integer> toBase = Argument
             .ofType(integer(2, 36));
 
