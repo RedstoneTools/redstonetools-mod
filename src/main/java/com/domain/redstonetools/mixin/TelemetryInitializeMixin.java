d45707055f497c2b282c98f7d624835205cb18bb
Matthias Wijnsma <matthiasx95@gmail.com>
2023-04-12 14:16:44 +0200

    Move telemetry client initialization

--- src/main/java/com/domain/redstonetools/mixin/TelemetryInitializeMixin.java
+++ src/main/java/com/domain/redstonetools/mixin/TelemetryInitializeMixin.java
@@ -1,7 +1,7 @@
 package com.domain.redstonetools.mixin;
 
 import com.domain.redstonetools.telemetry.TelemetryClient;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,9 +9,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static com.domain.redstonetools.RedstoneToolsClient.INJECTOR;
 
-@Mixin(TitleScreen.class)
-public class TitleScreenMixin {
-    @Inject(method = "init", at = @At("TAIL"))
+@Mixin(MinecraftClient.class)
+public class TelemetryInitializeMixin {
+    @Inject(method = "<init>", at = @At("TAIL"))
     public void init(CallbackInfo ci) {
         INJECTOR.getInstance(TelemetryClient.class);
     }
