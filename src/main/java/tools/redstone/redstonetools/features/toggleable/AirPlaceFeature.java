af5f0417cebfddd1403cdf105f8a7a48efcd1a72
Jay Mensink <j.mensink0206@gmail.com>
2023-06-03 21:57:15 +0200

    AirPlace Reach
    This issue required a little refactoring, as well as a server-side removal of the reach limit.

--- src/main/java/tools/redstone/redstonetools/features/toggleable/AirPlaceFeature.java
+++
@@ -1,8 +0,0 @@
-package tools.redstone.redstonetools.features.toggleable;
-
-import tools.redstone.redstonetools.features.Feature;
-
-@Feature(name = "Air Place", description = "Allows you to place blocks in the air.", command = "airplace")
-public class AirPlaceFeature extends ToggleableFeature {
-
-}
