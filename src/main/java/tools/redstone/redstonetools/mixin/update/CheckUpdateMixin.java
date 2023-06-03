64b7fc3c9632f8d9c3a7775d1afc5cadc5e14db2
Matthias Wijnsma <matthiasx95@gmail.com>
2023-06-03 15:36:28 +0200

    Use confirmscreen and fix url

--- src/main/java/tools/redstone/redstonetools/mixin/update/CheckUpdateMixin.java
+++ src/main/java/tools/redstone/redstonetools/mixin/update/CheckUpdateMixin.java
@@ -3,27 +3,26 @@ package tools.redstone.redstonetools.mixin.update;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import tools.redstone.redstonetools.RedstoneToolsClient;
-import tools.redstone.redstonetools.gui.UpdatePopupScreen;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 import static tools.redstone.redstonetools.RedstoneToolsClient.LOGGER;
+import static tools.redstone.redstonetools.RedstoneToolsClient.MOD_VERSION;
 
 @Mixin(TitleScreen.class)
-
 public class CheckUpdateMixin extends Screen {
     public boolean updateChecked = false;
 
@@ -39,22 +38,20 @@ public class CheckUpdateMixin extends Screen {
         try {
             LOGGER.info("Checking for updates...");
 
-            // timeout before aborting connection
-            // gh took quite long to respond on some machines
-            // so i think the timeout should be 1s
-            final long timeout = 1000;
-
             HttpClient client = HttpClient.newBuilder()
-                    .connectTimeout(Duration.of(timeout, ChronoUnit.MILLIS))
                     .build();
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.github.com/repos/RedstoneTools/redstonetools/releases/latest"))
+                    .uri(URI.create("https://api.github.com/repositories/597142955/releases/latest"))
                     .GET()
                     .build();
+
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             String responseBody = response.body();
-            if (response.statusCode() != 200)
+
+            if (response.statusCode() < 200 || 299 < response.statusCode()) {
+                LOGGER.error("Got status code " + response.statusCode() + " while trying to check for updates");
                 return;
+            }
 
             Gson gson = new Gson();
             JsonObject release = gson.fromJson(responseBody, JsonObject.class);
@@ -62,13 +59,28 @@ public class CheckUpdateMixin extends Screen {
             String newVersion = release.get("tag_name").getAsString();
 
             LOGGER.info("Found latest version: " + newVersion);
-            if (RedstoneToolsClient.MOD_VERSION.equals(newVersion) || newVersion.contains("alpha") || newVersion.contains("beta")) {
-                LOGGER.info("Already up to date, current version: " + RedstoneToolsClient.MOD_VERSION + ", new version: " + newVersion);
+            if (newVersion.contains("alpha") || newVersion.contains("beta")) {
+                LOGGER.info("Not showing an update popup for alpha or beta release, current version: " + MOD_VERSION + ", new version: " + newVersion);
+                return;
+            }
+
+            if (RedstoneToolsClient.MOD_VERSION.equals(newVersion)) {
+                LOGGER.info("Already up to date, current version: " + MOD_VERSION);
                 return;
             }
 
             LOGGER.info("Found newer version, current version: " + RedstoneToolsClient.MOD_VERSION + ", new version: " + newVersion);
-            MinecraftClient.getInstance().setScreen(new UpdatePopupScreen(this, uri, newVersion));
+
+            var parentScreen = MinecraftClient.getInstance().currentScreen;
+            var popup = new ConfirmScreen(confirmed -> {
+                MinecraftClient.getInstance().setScreen(parentScreen);
+
+                if (confirmed) {
+                    Util.getOperatingSystem().open(uri);
+                }
+            }, Text.of("Update Available"), Text.of("An update is available for redstone tools! You are on version " + MOD_VERSION + " but version " + newVersion + " is available."), Text.of("Go to release"), Text.of("Ignore"));
+
+            MinecraftClient.getInstance().setScreen(popup);
         } catch (Exception e) {
             LOGGER.warn("Failed to check for RedstoneTools updates");
             e.printStackTrace();
