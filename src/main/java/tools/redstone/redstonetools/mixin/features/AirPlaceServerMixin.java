78e76b045843ccd2347b36ce1d7a8a07eb297345
Jay Mensink <j.mensink0206@gmail.com>
2023-06-04 01:12:29 +0200

    Requested Changes
    do we *really* need to override the register method? This implementation is relatively clean anyway.

--- src/main/java/tools/redstone/redstonetools/mixin/features/AirPlaceServerMixin.java
+++ src/main/java/tools/redstone/redstonetools/mixin/features/AirPlaceServerMixin.java
@@ -4,13 +4,15 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import tools.redstone.redstonetools.RedstoneToolsClient;
+import tools.redstone.redstonetools.features.commands.AirPlaceFeature;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class AirPlaceServerMixin {
 
     @ModifyConstant(method = "onPlayerInteractBlock", constant = @Constant(doubleValue = 64.0) )
     private double modifyConstant(double originalValue) {
-        return 999;
+        return Double.POSITIVE_INFINITY;
     }
 
 }
