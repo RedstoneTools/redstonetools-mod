8eb7d4027469c7d0d35203b7e776b1547075f0b8
xtrm <oss@xtrm.me>
2023-06-04 01:43:13 +0200

    ✨ feature(di): switched the DI library to Doctor, simplified modules, and slashed dependencies

--- src/main/java/tools/redstone/redstonetools/di/FeatureModule.java
+++ src/main/java/tools/redstone/redstonetools/di/FeatureModule.java
@@ -1,17 +1,18 @@
 package tools.redstone.redstonetools.di;
 
 import com.google.auto.service.AutoService;
-import com.google.inject.AbstractModule;
+import rip.hippo.inject.DoctorModule;
+import rip.hippo.inject.binding.Binder;
 import tools.redstone.redstonetools.utils.ReflectionUtils;
 
-@AutoService(AbstractModule.class)
-public class FeatureModule extends AbstractModule {
+@AutoService(DoctorModule.class)
+public class FeatureModule implements DoctorModule {
     @SuppressWarnings({"rawtypes", "unchecked"}) // this is probably the only way to make it work
     @Override
-    protected void configure() {
+    public void configure(Binder binder) {
         for (var feature : ReflectionUtils.getFeatures()) {
             Class clazz = feature.getClass();
-            bind(clazz).toInstance(feature);
+            binder.bind(clazz).toInstance(feature);
         }
     }
 }
