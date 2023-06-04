625befc2b86dd9e72b6d89bb743f961848d971f3
Jay Mensink <j.mensink0206@gmail.com>
2023-06-04 12:43:07 +0200

    Air Place reach and toggle separation
    I've now kept toggling and setting reach to different features. This way they will register separately for GUIs, as per @Matthias1590's request.

--- src/main/java/tools/redstone/redstonetools/features/commands/AirPlaceReachFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/commands/AirPlaceReachFeature.java
@@ -9,30 +9,18 @@ import tools.redstone.redstonetools.features.feedback.Feedback;
 import static tools.redstone.redstonetools.features.arguments.serializers.FloatSerializer.floatArg;
 
 @Feature(name = "Air Place", description = "Allows you to place blocks in the air.", command = "airplace")
-public class AirPlaceFeature extends CommandFeature {
+public class AirPlaceReachFeature extends CommandFeature {
 
-    public boolean enabled;
     public float reach = 5.0f;
 
     public static final Argument<Float> distance = Argument
-        .ofType(floatArg(1.0f))
-        .withDefault(null);
-
-    private void toggle() {
-        enabled = !enabled;
-    }
+        .ofType(floatArg(1.0f));
 
     @Override
     protected Feedback execute(ServerCommandSource source) throws CommandSyntaxException {
 
-        // If no distance is supplied, resort to toggling the feature.
-        if ( distance.getValue() == null ) {
-            toggle();
-            return enabled ? Feedback.success("AirPlace has been enabled.") : Feedback.success("AirPlace has been disabled.");
-        }
-
         reach = distance.getValue();
-        return Feedback.success("AirPlace Reach set to " + distance.getValue().toString() + " blocks.");
+        return Feedback.success("Air Place reach set to " + distance.getValue().toString() + " blocks.");
 
     }
 }
