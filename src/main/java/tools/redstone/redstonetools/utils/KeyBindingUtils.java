b1cc52c778f326cc62bdf154b05d1e5103e17b11
Jay Mensink <j.mensink0206@gmail.com>
2023-06-04 16:40:58 +0200

    Duplicate Macro Bind Indicator
    If you try to bind a macro to a key that is already bound, it will indicate this by making the bind key red.

--- src/main/java/tools/redstone/redstonetools/utils/KeyBindingUtils.java
+++ src/main/java/tools/redstone/redstonetools/utils/KeyBindingUtils.java
@@ -1,12 +1,22 @@
 package tools.redstone.redstonetools.utils;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil.Key;
 import tools.redstone.redstonetools.macros.KeyBindingMixin;
 
 public class KeyBindingUtils {
 
     public static void removeKeyBinding(KeyBinding keyBinding) {
         ((KeyBindingMixin)keyBinding).removeKeybinding(keyBinding);
+    }
+
+    public static boolean isKeyAlreadyBound(Key key) {
+        KeyBinding[] keyBindings = MinecraftClient.getInstance().options.allKeys;
+        for (KeyBinding keyBinding : keyBindings) {
+            if (keyBinding.getDefaultKey() == key) { return true; }
+        }
+        return false;
     }
 
 }
