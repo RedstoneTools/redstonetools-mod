87c31db373a4db1620a652486596670913360875
Dav32_1 <64831818+Dav321@users.noreply.github.com>
2023-06-04 20:15:10 +0200

    Add the ability to use the attack key in combination with AirPlace

--- src/main/java/tools/redstone/redstonetools/mixin/features/AirplaceMixin.java
+++ src/main/java/tools/redstone/redstonetools/mixin/features/AirplaceMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import tools.redstone.redstonetools.RedstoneToolsClient;
 import tools.redstone.redstonetools.features.toggleable.AirPlaceFeature;
@@ -23,25 +24,60 @@ public class AirplaceMixin {
 
     @Inject(method = "doItemUse", at = @At(value = "HEAD"), locals = LocalCapture.CAPTURE_FAILHARD)
     public void doItemUse(CallbackInfo callbackInfo) {
+        if (!isAirplaceAllowed()) {
+            return;
+        }
+
+        var hitResult = getAirplaceHitResult();
+
+        crosshairTarget = new BlockHitResult(hitResult.getPos(), Direction.UP, new BlockPos(hitResult.getPos()), false);
+    }
+
+    @Inject(method = "doAttack", at = @At(value = "HEAD"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void doAttack(CallbackInfoReturnable<Boolean> cir) {
+        if (!isAirplaceAllowed()) {
+            return;
+        }
+
+        var hitResult = getAirplaceHitResult();
+        var minecraftClient = (MinecraftClient) (Object) this;
+
+        //Call interactionManager directly because the block is air, with which the player cannot interact
+        minecraftClient.interactionManager.attackBlock(new BlockPos(hitResult.getPos()), Direction.UP);
+    }
+
+    private boolean isAirplaceAllowed() {
         // If airplace is disabled
         if (!airPlaceFeature.isEnabled()) {
-            return;
+            return false;
         }
 
         // If the hit result is already set
         if (crosshairTarget != null && crosshairTarget.getType() != HitResult.Type.MISS) {
-            return;
+            return false;
         }
 
-        var player = ((MinecraftClient) (Object) this).player;
-        var interactionManager = ((MinecraftClient) (Object) this).interactionManager;
+        // If the player or interactionManager not initialized
+        var minecraftClient = (MinecraftClient) (Object) this;
+        var player = minecraftClient.player;
+        var interactionManager = minecraftClient.interactionManager;
         if (player == null || interactionManager == null) {
-            return;
+            return false;
         }
 
-        var reach = interactionManager.getReachDistance();
-        var hitResult = player.raycast(reach, 0, false);
+        return true;
+    }
 
-        crosshairTarget = new BlockHitResult(hitResult.getPos(), Direction.UP, new BlockPos(hitResult.getPos()), false);
+    private HitResult getAirplaceHitResult() {
+        var minecraftClient = (MinecraftClient) (Object) this;
+
+        // Asserting values previously tested by isAirplaceAllowed()
+        assert minecraftClient.interactionManager != null;
+        assert minecraftClient.player != null;
+
+        var reach = minecraftClient.interactionManager.getReachDistance();
+        var hitResult = minecraftClient.player.raycast(reach, 0, false);
+
+        return hitResult;
     }
 }
