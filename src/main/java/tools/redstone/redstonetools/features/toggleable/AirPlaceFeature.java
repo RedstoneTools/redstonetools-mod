c38a6ddabf4fa775c914d0d6364c2960ed53c160
Jay Mensink <j.mensink0206@gmail.com>
2023-06-04 12:48:41 +0200

    Merge Conflict Resolved

--- src/main/java/tools/redstone/redstonetools/features/toggleable/AirPlaceFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/toggleable/AirPlaceFeature.java
@@ -1,7 +1,6 @@
 package tools.redstone.redstonetools.features.toggleable;
 
 import tools.redstone.redstonetools.features.Feature;
-import tools.redstone.redstonetools.features.commands.CommandFeature;
 
 @Feature(name = "Air Place", description = "Allows you to place blocks in the air.", command = "airplace")
 public class AirPlaceFeature extends ToggleableFeature {
