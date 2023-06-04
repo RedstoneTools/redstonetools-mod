3b2be0eb8ff5cc51a770bf73fff4be96f1cc9f07
Jay Mensink <j.mensink0206@gmail.com>
2023-06-04 12:43:07 +0200

    Air Place reach and toggle separation
    I've now kept toggling and setting reach to different features. This way they will register separately for GUIs, as per @Matthias1590's request.

---
+++ src/main/java/tools/redstone/redstonetools/features/toggleable/AirPlaceFeature.java
@@ -0,0 +1,9 @@
+package tools.redstone.redstonetools.features.toggleable;
+
+import tools.redstone.redstonetools.features.Feature;
+import tools.redstone.redstonetools.features.commands.CommandFeature;
+
+@Feature(name = "Air Place", description = "Allows you to place blocks in the air.", command = "airplace")
+public class AirPlaceFeature extends ToggleableFeature {
+
+}
