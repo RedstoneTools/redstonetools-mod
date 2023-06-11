19243211437668c2cb46778b6e4a087e85979472
Orbyfied <officialglitchout@gmail.com>
2023-06-11 19:22:31 +0200

    move to new toggleables and optimize BigDustFeature

--- src/main/java/tools/redstone/redstonetools/features/toggleable/BigDustFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/toggleable/BigDustFeature.java
@@ -3,8 +3,15 @@ package tools.redstone.redstonetools.features.toggleable;
 import com.google.auto.service.AutoService;
 import tools.redstone.redstonetools.features.AbstractFeature;
 import tools.redstone.redstonetools.features.Feature;
+import tools.redstone.redstonetools.features.arguments.Argument;
+import tools.redstone.redstonetools.features.arguments.serializers.IntegerSerializer;
 
 @AutoService(AbstractFeature.class)
 @Feature(name = "Big Dust", description = "Change the size of redstone's hitbox.", command = "bigdust")
 public class BigDustFeature extends ToggleableFeature {
+
+    public static final Argument<Integer> heightInPixels = Argument.ofType(IntegerSerializer.integer(1, 16))
+            .withDefault(1);
+
+
 }
