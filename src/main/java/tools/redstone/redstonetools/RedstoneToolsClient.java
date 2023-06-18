83aacee11d55ef0f0c81dcea7c7b540ce2dc3833
killian <26600206+xtrm-en@users.noreply.github.com>
2023-06-19 00:57:03 +0200

    Update RedstoneToolsClient.java
    bruh

--- src/main/java/tools/redstone/redstonetools/RedstoneToolsClient.java
+++ src/main/java/tools/redstone/redstonetools/RedstoneToolsClient.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rip.hippo.inject.Doctor;
 import rip.hippo.inject.Injector;
-import tools.redstone.redstonetools.macros.WorldlessCommandHelper;
 import tools.redstone.redstonetools.utils.DependencyLookup;
 import tools.redstone.redstonetools.utils.ReflectionUtils;
 
