42ac995f05c4625a0ccb5bc613631abd386b6eb2
Orbyfied <officialglitchout@gmail.com>
2023-06-24 13:11:10 +0200

    fixed crash with reflection for drawBlockOutline

--- src/main/java/tools/redstone/redstonetools/features/toggleable/AirPlaceFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/toggleable/AirPlaceFeature.java
@@ -9,9 +9,6 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -24,31 +21,15 @@ import tools.redstone.redstonetools.features.AbstractFeature;
 import tools.redstone.redstonetools.features.Feature;
 import tools.redstone.redstonetools.features.arguments.Argument;
 import tools.redstone.redstonetools.features.arguments.serializers.BoolSerializer;
+import tools.redstone.redstonetools.mixin.accessors.WorldRendererAccessor;
 import tools.redstone.redstonetools.utils.ItemUtils;
 import tools.redstone.redstonetools.utils.RaycastUtils;
-import tools.redstone.redstonetools.utils.ReflectionUtils;
-
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodType;
 
 import static tools.redstone.redstonetools.features.arguments.serializers.FloatSerializer.floatArg;
 
 @AutoService(AbstractFeature.class)
 @Feature(name = "Air Place", description = "Allows you to place blocks in the air.", command = "airplace")
 public class AirPlaceFeature extends ToggleableFeature {
-
-    static MethodHandle methodDrawBlockOutline;
-
-    static {
-        try {
-            methodDrawBlockOutline = ReflectionUtils.getInternalLookup()
-                    .findVirtual(WorldRenderer.class, "drawBlockOutline",
-                            MethodType.methodType(void.class, MatrixStack.class, VertexConsumer.class, Entity.class,
-                                    double.class, double.class, double.class, BlockPos.class, BlockState.class));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static boolean canAirPlace(PlayerEntity player) {
         ItemStack itemStack = ItemUtils.getMainItem(player);
@@ -124,11 +105,11 @@ public class AirPlaceFeature extends ToggleableFeature {
 
             try {
                 VertexConsumer consumer = context.consumers().getBuffer(RenderLayer.getLines());
-                methodDrawBlockOutline.invoke(
-                        context.worldRenderer(),
+
+                ((WorldRendererAccessor)context.worldRenderer()).invokeDrawBlockOutline(
                         context.matrixStack(),
                         consumer,
-                        (Entity)client.player,
+                        client.player,
                         camPos.x, camPos.y, camPos.z,
                         blockPos,
                         blockState
