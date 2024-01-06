ced8b64cd5925fab1eaf18897aeb27f55123851c
rugmj <rugmj@rugmj.dev>
2024-01-06 20:18:30 +0000

    defines required interfaces for argument rewrite

--- src/main/java/tools/redstone/redstonetools/RedstoneToolsClient.java
+++ src/main/java/tools/redstone/redstonetools/RedstoneToolsClient.java
@@ -1,12 +1,23 @@
 package tools.redstone.redstonetools;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.command.argument.serialize.ArgumentSerializer;
+import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
+import net.minecraft.util.Identifier;
+
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.mojang.brigadier.arguments.ArgumentType;
+
 import rip.hippo.inject.Doctor;
 import rip.hippo.inject.Injector;
+import tools.redstone.redstonetools.features.arguments.serializers.IntegerSerializer;
+import tools.redstone.redstonetools.features.arguments.serializers.TypeSerializer;
 import tools.redstone.redstonetools.utils.DependencyLookup;
 import tools.redstone.redstonetools.utils.ReflectionUtils;
 
@@ -20,6 +31,7 @@ public class RedstoneToolsClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final Path CONFIG_DIR = FabricLoader.getInstance().getConfigDir().resolve("redstonetools");
     public static final Injector INJECTOR = Doctor.createInjector(ReflectionUtils.getModules());
+    public static final Reflections REFLECTIONS = new Reflections("tools.redstone.redstonetools");
 
     @Override
     public void onInitializeClient() {
@@ -29,7 +41,9 @@ public class RedstoneToolsClient implements ClientModInitializer {
         RedstoneToolsGameRules.register();
 
         // Register features
-        ReflectionUtils.getFeatures().forEach(feature -> {
+        ReflectionUtils.getFeatures().forEach(feature ->
+
+        {
             LOGGER.trace("Registering feature {}", feature.getClass().getName());
 
             if (feature.requiresWorldEdit() && !DependencyLookup.WORLDEDIT_PRESENT) {
