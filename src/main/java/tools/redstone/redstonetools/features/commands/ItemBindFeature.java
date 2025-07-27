fa7cda2d9e6d1a835b9c657fb2a1c29b5ad1ee0c
kr1v <kravpolet@gmail.com>
2025-07-27 11:19:17 +0200

    Fixed up ItemBindFeature

--- src/main/java/tools/redstone/redstonetools/features/commands/ItemBindFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/commands/ItemBindFeature.java
@@ -32,9 +32,10 @@ public class ItemBindFeature extends AbstractFeature {
                     if (BoolArgumentType.getBool(context, "reset")) {
                         if (context.getSource().getPlayer() != null) {
                             context.getSource().getPlayer().getMainHandStack().remove(RedstoneToolsClient.COMMAND_COMPONENT);
-                            context.getSource().sendMessage(Text.of("You need to be holding an item in your main hand!"));
-                        } else {
+                            context.getSource().getPlayer().getMainHandStack().remove(DataComponentTypes.LORE);
                             context.getSource().sendMessage(Text.of("Successfully removed command from the item in your main hand"));
+                        } else {
+                            context.getSource().sendMessage(Text.of("You need to be holding an item in your main hand!"));
                         }
                         return 1;
                     } else {
