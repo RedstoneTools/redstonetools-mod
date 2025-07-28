422c7fbaa6a2db4906fa554afe7bdb4b4fda7308
kr1v <kravpolet@gmail.com>
2025-07-28 18:46:48 +0200

    Check if worldedit exists before loading worldedit related features

--- src/main/java/tools/redstone/redstonetools/macros/Commands.java
+++ src/main/java/tools/redstone/redstonetools/macros/Commands.java
@@ -1,15 +1,11 @@
 package tools.redstone.redstonetools.macros;
 
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.minecraft.client.MinecraftClient;
 import tools.redstone.redstonetools.features.commands.*;
 import tools.redstone.redstonetools.features.commands.update.UpdateFeature;
 import tools.redstone.redstonetools.features.toggleable.AirPlaceFeature;
 import tools.redstone.redstonetools.features.toggleable.AutoDustFeature;
 import tools.redstone.redstonetools.features.toggleable.BigDustFeature;
-import tools.redstone.redstonetools.macros.gui.malilib.MacrosScreen;
-
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback.*;
+import tools.redstone.redstonetools.utils.DependencyLookup;
 
 public class Commands {
 	public static void registerCommands() {
@@ -20,17 +16,20 @@ public class Commands {
 //				})));
 		// for some reason setScreen doesn't work in commands. sucks.
 
-		BinaryBlockReadFeature.registerCommand();
+		if (DependencyLookup.WORLDEDIT_PRESENT) {
+			UpdateFeature.registerCommand();
+			BinaryBlockReadFeature.registerCommand();
+			ColorCodeFeature.registerCommand();
+			MinSelectionFeature.registerCommand();
+			RStackFeature.registerCommand();
+		}
+
 		BaseConvertFeature.registerCommand();
-		UpdateFeature.registerCommand();
-		ColorCodeFeature.registerCommand();
 		ColoredFeature.registerCommand();
 		CopyStateFeature.registerCommand();
 		ItemBindFeature.registerCommand();
 		MacroFeature.registerCommand();
-		MinSelectionFeature.registerCommand();
 		QuickTpFeature.registerCommand();
-		RStackFeature.registerCommand();
 		SignalStrengthBlockFeature.registerCommand();
 		GiveMeFeature.registerCommand();
 		AirPlaceFeature.registerCommand();
