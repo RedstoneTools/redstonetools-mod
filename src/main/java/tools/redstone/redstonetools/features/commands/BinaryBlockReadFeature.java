824c94e23fe725656a948d66441d85dfa947c6c7
kr1v <kravpolet@gmail.com>
2025-07-29 11:57:36 +0200

    Binary block read works, now with optional arguments

--- src/main/java/tools/redstone/redstonetools/features/commands/BinaryBlockReadFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/commands/BinaryBlockReadFeature.java
@@ -8,8 +8,10 @@ import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.Region;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RedstoneLampBlock;
 import net.minecraft.command.argument.BlockStateArgumentType;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
@@ -17,22 +19,58 @@ import tools.redstone.redstonetools.features.AbstractFeature;
 import tools.redstone.redstonetools.utils.FeatureUtils;
 import tools.redstone.redstonetools.utils.WorldEditUtils;
 
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
 public class BinaryBlockReadFeature extends AbstractFeature {
     public static void registerCommand() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal("binaryblockread")
-                .then(CommandManager.argument("offset", IntegerArgumentType.integer(1))
-                .then(CommandManager.argument("onBlock", BlockStateArgumentType.blockState(registryAccess))
-                .then(CommandManager.argument("toBase", IntegerArgumentType.integer())
-                .then(CommandManager.argument("reverseBits", BoolArgumentType.bool())
-                .executes(context -> FeatureUtils.getFeature(BinaryBlockReadFeature.class).execute(context))))))));
+        BinaryBlockReadFeature bbr = FeatureUtils.getFeature(BinaryBlockReadFeature.class);
+        CommandRegistrationCallback.EVENT.register(
+                (dispatcher, registryAccess, environment) -> dispatcher.register(
+                literal("binaryblockread")
+                    .executes(bbr::parseArgs)
+                .then(argument("offset", IntegerArgumentType.integer(1))
+                    .executes(bbr::parseArgs)
+                .then(argument("onBlock", BlockStateArgumentType.blockState(registryAccess))
+                    .executes(bbr::parseArgs)
+                .then(argument("toBase", IntegerArgumentType.integer())
+                    .executes(bbr::parseArgs)
+                .then(argument("reverseBits", BoolArgumentType.bool())
+                    .executes(bbr::parseArgs)
+                )))))
+        );
     }
 
-    protected int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        var offset = IntegerArgumentType.getInteger(context, "offset");
-        var onBlock = BlockStateArgumentType.getBlockState(context, "onBlock");
-        var toBase = IntegerArgumentType.getInteger(context, "toBase");
-        var reverseBits = BoolArgumentType.getBool(context, "reverseBits");
+    // this is horrible, but it works
+    protected int parseArgs(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        int offset;
+        BlockState onBlock;
+        int toBase;
+        boolean reverseBits;
+        try {
+            offset = IntegerArgumentType.getInteger(context, "offset");
+        } catch (Exception ignored) {
+            offset = 2;
+        }
+        try {
+            onBlock = BlockStateArgumentType.getBlockState(context, "onBlock").getBlockState();
+        } catch (Exception ignored) {
+            onBlock = Blocks.REDSTONE_LAMP.getDefaultState().with(RedstoneLampBlock.LIT, true);
+        }
+        try {
+            toBase = IntegerArgumentType.getInteger(context, "toBase");
+        } catch (Exception ignored) {
+            toBase = 10;
+        }
+        try {
+            reverseBits = BoolArgumentType.getBool(context, "reverseBits");
+        } catch (Exception ignored) {
+            reverseBits = false;
+        }
+        return execute(context, offset, onBlock, toBase, reverseBits);
+    }
 
+    protected int execute(CommandContext<ServerCommandSource> context, int offset, BlockState onBlock, int toBase, boolean reverseBits) throws CommandSyntaxException {
         var source = context.getSource();
         Region selection = WorldEditUtils.getSelection(source.getPlayer());
 
@@ -57,10 +95,17 @@ public class BinaryBlockReadFeature extends AbstractFeature {
             var pos = new BlockPos(point.x(), point.y(), point.z());
             var actualState = source.getWorld().getBlockState(pos);
 
-            var matches = !actualState.equals(onBlock.getBlockState());
+            var matches = actualState.equals(onBlock);
             if (matches) {
                 for (var property : onBlock.getProperties()) {
-                    var propertyValue = onBlock.getBlockState().get(property);
+                    try {
+                        actualState.get(property);
+                    } catch (Exception e) { // actualState doesn't have the same properties as onBlock
+                                            // so they dont match by default.
+                        matches = false;
+                        break;
+                    }
+                    var propertyValue = onBlock.get(property);
 
                     if (!actualState.get(property).equals(propertyValue)) {
                         matches = false;
@@ -77,7 +122,7 @@ public class BinaryBlockReadFeature extends AbstractFeature {
         }
 
         var output = Integer.toString(Integer.parseInt(bits.toString(), 2), toBase);
-        source.getPlayer().sendMessage(Text.of(output));
+        source.sendMessage(Text.of(output));
         return 0;
     }
 }
