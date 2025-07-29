6f48a320c41d0b10897848086e80cbba9fa6a909
kr1v <kravpolet@gmail.com>
2025-07-29 15:01:21 +0200

    Readd /edit-macros, fix transparent text in macros screen

--- src/main/java/tools/redstone/redstonetools/macros/Commands.java
+++ src/main/java/tools/redstone/redstonetools/macros/Commands.java
@@ -1,20 +1,24 @@
 package tools.redstone.redstonetools.macros;
 
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.minecraft.client.MinecraftClient;
 import tools.redstone.redstonetools.features.commands.*;
 import tools.redstone.redstonetools.features.commands.update.UpdateFeature;
 import tools.redstone.redstonetools.features.toggleable.AirPlaceFeature;
 import tools.redstone.redstonetools.features.toggleable.AutoDustFeature;
 import tools.redstone.redstonetools.features.toggleable.BigDustFeature;
+import tools.redstone.redstonetools.macros.gui.malilib.MacrosScreen;
 import tools.redstone.redstonetools.utils.DependencyLookup;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback.EVENT;
 
 public class Commands {
 	public static void registerCommands() {
-//		EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("edit-macros")
-//				.executes(commandContext -> {
-//					MinecraftClient.getInstance().setScreen(new MacrosScreen(MinecraftClient.getInstance().currentScreen, MacroManager.getMacros()));
-//					return 1;
-//				})));
-		// for some reason setScreen doesn't work in commands. sucks.
+		EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("edit-macros")
+				.executes(commandContext -> {
+					MinecraftClient.getInstance().send(() -> MinecraftClient.getInstance().setScreen(new MacrosScreen(MinecraftClient.getInstance().currentScreen, MacroManager.getMacros())));
+					return 1;
+				})));
 
 		if (DependencyLookup.WORLDEDIT_PRESENT) {
 			UpdateFeature.registerCommand();
