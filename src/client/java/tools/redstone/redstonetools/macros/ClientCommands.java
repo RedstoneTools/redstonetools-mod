6094319a6b3627c500546a75818705db4f2d6bfc
kr1v <kravpolet@gmail.com>
2025-07-30 17:08:57 +0200

    Extract /edit-macros to its own class

--- src/client/java/tools/redstone/redstonetools/macros/ClientCommands.java
+++ src/client/java/tools/redstone/redstonetools/macros/ClientCommands.java
@@ -1,25 +1,14 @@
 package tools.redstone.redstonetools.macros;
 
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.minecraft.client.MinecraftClient;
 import tools.redstone.redstonetools.features.commands.*;
 import tools.redstone.redstonetools.features.commands.update.UpdateFeature;
 import tools.redstone.redstonetools.features.toggleable.AirPlaceFeature;
 import tools.redstone.redstonetools.features.toggleable.AutoDustFeature;
 import tools.redstone.redstonetools.features.toggleable.BigDustFeature;
-import tools.redstone.redstonetools.macros.gui.malilib.MacrosScreen;
 import tools.redstone.redstonetools.utils.DependencyLookup;
-
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback.EVENT;
 
 public class ClientCommands {
 	public static void registerCommands() {
-		EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("edit-macros")
-				.executes(commandContext -> {
-					MinecraftClient.getInstance().send(() -> MinecraftClient.getInstance().setScreen(new MacrosScreen(MinecraftClient.getInstance().currentScreen, MacroManager.getMacros())));
-					return 1;
-				})));
-
 		if (DependencyLookup.WORLDEDIT_PRESENT) {
 			UpdateFeature.registerCommand();
 			BinaryBlockReadFeature.registerCommand();
@@ -31,8 +20,7 @@ public class ClientCommands {
 		ColoredFeature.registerCommand();
 		CopyStateFeature.registerCommand();
 		BaseConvertFeature.registerCommand();
-		ColoredFeature.registerCommand();
-		CopyStateFeature.registerCommand();
+		EditMacroFeature.registerCommand();
 		MacroFeature.registerCommand();
 		QuickTpFeature.registerCommand();
 		SignalStrengthBlockFeature.registerCommand();
