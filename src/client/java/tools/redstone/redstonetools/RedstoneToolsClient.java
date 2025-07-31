f45b4b848d6dc06424222ca35277ded8d6ccaa4c
kr1v <kravpolet@gmail.com>
2025-07-31 15:39:47 +0200

    Remove unused methods

--- src/client/java/tools/redstone/redstonetools/RedstoneToolsClient.java
+++ src/client/java/tools/redstone/redstonetools/RedstoneToolsClient.java
@@ -6,13 +6,9 @@ import org.lwjgl.util.tinyfd.TinyFileDialogs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tools.redstone.redstonetools.macros.ClientCommands;
-import tools.redstone.redstonetools.macros.gui.malilib.InitHandler;
-
-import java.nio.file.Path;
 
 public class RedstoneToolsClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(RedstoneTools.MOD_ID);
-    public static final Path CONFIG_DIR = FabricLoader.getInstance().getConfigDir().resolve("redstonetools");
     @Override
     public void onInitializeClient() {
         LOGGER.info("Initializing Redstone Tools");
@@ -31,7 +27,6 @@ public class RedstoneToolsClient implements ClientModInitializer {
         // Register commands
         ClientCommands.registerCommands();
         Commands.registerCommands();
-        InitHandler.initHandlerExecuter();
 //        Registry.register(Registries.COMMAND_ARGUMENT_TYPE, Identifier.of("blockcolor"), new BlockColorArgumentSerializer());
     }
 }
--- src/main/java/tools/redstone/redstonetools/utils/WorldEditUtils.java
+++ src/main/java/tools/redstone/redstonetools/utils/WorldEditUtils.java
@@ -5,49 +5,11 @@ import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.sk89q.worldedit.IncompleteRegionException;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.fabric.FabricAdapter;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
-import java.util.function.Consumer;
-
 public class WorldEditUtils {
-    /**
-     * Execute a function for each block in
-     * the provided region.
-     * <p>
-     * Iterates the bounding box of the region
-     * and checks if the position is contained
-     * for each block.
-     * <p>
-     * TODO: maybe make an async version of this somehow
-     *
-     * @param region The region.
-     * @param consumer The function to run.
-     */
-    public static void forEachBlockInRegion(Region region,
-                                            Consumer<BlockVector3> consumer) {
-        if (!DependencyLookup.WORLDEDIT_PRESENT) {
-            throw new IllegalStateException("WorldEdit is not loaded.");
-        }
-
-        CuboidRegion bb = region.getBoundingBox();
-        BlockVector3 min = bb.getMinimumPoint();
-        BlockVector3 max = bb.getMaximumPoint();
-        for (int x = min.x(); x <= max.x(); x++) {
-            for (int y = min.y(); y <= max.y(); y++) {
-                for (int z = min.z(); z <= max.z(); z++) {
-                    BlockVector3 vec = BlockVector3.at(x, y, z);
-                    if (!region.contains(vec))
-                        continue;
-                    consumer.accept(vec);
-                }
-            }
-        }
-    }
-
     public static Region getSelection(ServerPlayerEntity player) throws CommandSyntaxException {
         if (!DependencyLookup.WORLDEDIT_PRESENT) {
             throw new IllegalStateException("WorldEdit is not loaded.");
