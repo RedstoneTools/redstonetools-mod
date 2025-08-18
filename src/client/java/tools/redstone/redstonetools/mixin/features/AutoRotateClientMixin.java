43c3b0e8ddc73962e97fc5dd5bf1f5f01826039a
kr1viah_ <kravpolet@gmail.com>
2025-08-18 11:50:50 +0200

    Fix hopper autorotate crash

--- src/client/java/tools/redstone/redstonetools/mixin/features/AutoRotateClientMixin.java
+++ src/client/java/tools/redstone/redstonetools/mixin/features/AutoRotateClientMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,7 +29,8 @@ public abstract class AutoRotateClientMixin {
 			original= original.with(Properties.HORIZONTAL_FACING, original.get(Properties.HORIZONTAL_FACING).getOpposite());
 
 		if (original.contains(Properties.HOPPER_FACING))
-			original= original.with(Properties.HOPPER_FACING, original.get(Properties.HOPPER_FACING).getOpposite());
+			if (original.get(Properties.HOPPER_FACING).getOpposite() != Direction.UP)
+				original= original.with(Properties.HOPPER_FACING, original.get(Properties.HOPPER_FACING).getOpposite());
 
 		return this.canPlace(context, original) ? original : null;
 	}
