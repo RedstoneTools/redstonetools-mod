394130a70764b1f73ee0ceae29ff0f6975bb74f8
kr1viah_ <kravpolet@gmail.com>
2025-08-18 17:16:05 +0200

    Safer /quicktp

--- src/main/java/tools/redstone/redstonetools/features/commands/QuickTpFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/commands/QuickTpFeature.java
@@ -4,8 +4,11 @@ import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
@@ -13,6 +16,9 @@ import tools.redstone.redstonetools.features.AbstractFeature;
 import tools.redstone.redstonetools.utils.FeatureUtils;
 import tools.redstone.redstonetools.utils.PositionUtils;
 import tools.redstone.redstonetools.utils.RaycastUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -30,7 +36,11 @@ public class QuickTpFeature extends AbstractFeature {
 										.executes(qtp::parseArguments))))));
 	}
 
-	protected int parseArguments(CommandContext<ServerCommandSource> context) {
+	protected int parseArguments(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		var player = context.getSource().getPlayer();
+
+		if (quicktpingForPlayer.contains(player)) throw new SimpleCommandExceptionType(Text.literal("Already doing a quicktp!")).create();
+		quicktpingForPlayer.add(player);
 		double distance;
 		boolean includeFluids;
 		boolean resetVelocity;
@@ -55,27 +65,43 @@ public class QuickTpFeature extends AbstractFeature {
 		Thread thread = new Thread(() -> {
 			try {
 				execute(context, finalDistance, finalIncludeFluids, finalResetVelocity);
-			} catch (CommandSyntaxException ignored) {
-
+			} catch (CommandSyntaxException e) {
+				throw new RuntimeException(e);
 			}
 		});
 		thread.start();
+		Thread t2 = new Thread(() -> {
+			try {
+				thread.join(10000);
+			} catch (InterruptedException ignored) {
+			}
+			if (thread.isAlive()) {
+				player.sendMessage(Text.literal("Quicktp still running after 10 seconds. Canceling quicktp!"));
+				quicktpingForPlayer.remove(player);
+			}
+			thread.interrupt();
+		});
+		t2.start();
 		return 1;
 	}
 
+	public static List<PlayerEntity> quicktpingForPlayer = new ArrayList<>();
+
 	protected static void execute(CommandContext<ServerCommandSource> context, double distance, boolean includeFluids, boolean resetVelocity) throws CommandSyntaxException {
 		var player = context.getSource().getPlayer();
-
 		assert player != null;
+		try {
+			var hit = player.raycast(distance, 0, includeFluids);
 
-		var hit = player.raycast(distance, 0, includeFluids);
+			var targetPosition = clampHitPosition(hit);
 
-		var targetPosition = clampHitPosition(hit);
-
-		player.requestTeleport(targetPosition.x, targetPosition.y, targetPosition.z);
-		if (resetVelocity) player.setVelocity(Vec3d.ZERO);
-		player.fallDistance = 0;
-		player.velocityModified = true; // guh
+			player.requestTeleport(targetPosition.x, targetPosition.y, targetPosition.z);
+			if (resetVelocity) player.setVelocity(Vec3d.ZERO);
+			player.fallDistance = 0;
+			player.velocityModified = true; // guh
+		} finally {
+			quicktpingForPlayer.remove(player);
+		}
 	}
 
 	private static Vec3d clampHitPosition(HitResult hit) {
