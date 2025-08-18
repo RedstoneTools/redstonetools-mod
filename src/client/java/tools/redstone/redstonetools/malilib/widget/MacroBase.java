f734036ddbbad02f3184bd84703243cde775a579
kr1viah_ <kravpolet@gmail.com>
2025-08-18 17:31:17 +0200

    Safer macros

--- src/client/java/tools/redstone/redstonetools/malilib/widget/MacroBase.java
+++ src/client/java/tools/redstone/redstonetools/malilib/widget/MacroBase.java
@@ -4,12 +4,14 @@ import fi.dy.masa.malilib.config.options.ConfigHotkey;
 import fi.dy.masa.malilib.event.InputEventHandler;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 import tools.redstone.redstonetools.macros.actions.Action;
 import tools.redstone.redstonetools.macros.actions.CommandAction;
 import tools.redstone.redstonetools.malilib.KeybindHandler;
 
 import java.util.AbstractList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MacroBase {
 	protected final MinecraftClient mc;
@@ -89,13 +91,28 @@ public class MacroBase {
 		return this.enabled;
 	}
 
+	private final AtomicInteger layers = new AtomicInteger(0);
+
 	public void run() {
-		if (!enabled) {
+		if (!enabled) return;
+		if (layers.getAndSet(layers.get() + 1) > 100) {
+			MinecraftClient.getInstance().player.sendMessage(Text.of("Please don't cause a stackoverflow :("), false);
 			return;
 		}
-
-		for (Action action : actions) {
-			action.run();
+		try {
+			for (Action action : actions) {
+				action.run();
+			}
+		} catch (StackOverflowError ignored) {
+			try {
+				MinecraftClient.getInstance().player.sendMessage(Text.of("Please don't cause a stackoverflow :("), false);
+			} catch (NoClassDefFoundError e) {
+				// yeah we are absolutely cooked, there is no way to recover from this. I'm not even sure this can happen
+				// actually there's probably a better throwable to be thrown here. whatever.
+				throw new InternalError("Something has gone terribly wrong. Shouldn't have run a macro that runs itself.", e);
+			}
+		} finally {
+			layers.set(0);
 		}
 	}
 }
