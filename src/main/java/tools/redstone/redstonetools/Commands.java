4527d566ddf1a9ac952a29cc14022e1b4af82b9e
kr1viah_ <kravpolet@gmail.com>
2025-08-21 17:33:54 +0200

    Add /component

--- src/main/java/tools/redstone/redstonetools/Commands.java
+++ src/main/java/tools/redstone/redstonetools/Commands.java
@@ -14,6 +14,7 @@ public class Commands {
 			MinSelectionFeature.registerCommand();
 			RStackFeature.registerCommand();
 		}
+		ItemComponentsFeature.registerCommand();
 		ItemBindFeature.registerCommand();
 		QuickTpFeature.registerCommand();
 		SignalStrengthBlockFeature.registerCommand();
