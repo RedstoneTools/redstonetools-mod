f88c4bd0a8f72007fb1bb68533c5b77af5700518
7410 <85879080+O7410@users.noreply.github.com>
2025-08-22 03:32:58 +0300

    /reach-block -> /reach block, added /reach reset, /reach block now suggests default value of the attribute (same for /reach entity)

--- src/client/java/tools/redstone/redstonetools/features/commands/ReachFeature.java
+++ src/client/java/tools/redstone/redstonetools/features/commands/ReachFeature.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.entity.attribute.EntityAttributes;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback.EVENT;
 
@@ -14,35 +16,46 @@ public class ReachFeature {
 	}
 
 	public void registerCommand() {
-		EVENT.register((dispatcher, registryAccess) -> {
+		EVENT.register((dispatcher, registryAccess) ->
 			dispatcher.register(ClientCommandManager.literal("reach")
+				.then(ClientCommandManager.argument("reach", FloatArgumentType.floatArg(0.0f))
+					.executes(context -> execute(context, true, true))
+				)
+				.then(ClientCommandManager.literal("reset")
+					.executes(context -> reset(context, true, true))
+				)
+				.then(ClientCommandManager.literal("block")
 					.then(ClientCommandManager.argument("reach", FloatArgumentType.floatArg(0.0f))
-							.executes(this::execute)));
-
-			dispatcher.register(ClientCommandManager.literal("reach-block")
+						.suggests((context, builder) -> {
+							builder.suggest(String.valueOf(EntityAttributes.BLOCK_INTERACTION_RANGE.value().getDefaultValue()));
+							return builder.buildFuture();
+						})
+						.executes(context -> execute(context, true, false))
+					)
+				)
+				.then(ClientCommandManager.literal("entity")
 					.then(ClientCommandManager.argument("reach", FloatArgumentType.floatArg(0.0f))
-							.executes(context -> {
-								float reach = FloatArgumentType.getFloat(context, "reach");
-
-								context.getSource().getPlayer().networkHandler.sendChatCommand("attribute @s minecraft:block_interaction_range base set " + reach);
-								return 1;
-							})));
-
-			dispatcher.register(ClientCommandManager.literal("reach-entity")
-					.then(ClientCommandManager.argument("reach", FloatArgumentType.floatArg(0.0f))
-							.executes(context -> {
-								float reach = FloatArgumentType.getFloat(context, "reach");
-								context.getSource().getPlayer().networkHandler.sendChatCommand("attribute @s minecraft:entity_interaction_range base set " + reach);
-								return 1;
-							})));
-
-		});
+						.suggests((context, builder) -> {
+							builder.suggest(String.valueOf(EntityAttributes.ENTITY_INTERACTION_RANGE.value().getDefaultValue()));
+							return builder.buildFuture();
+						})
+						.executes(context -> execute(context, false, true))
+					)
+				)));
 	}
 
-	private int execute(CommandContext<FabricClientCommandSource> context) {
+	private int execute(CommandContext<FabricClientCommandSource> context, boolean block, boolean entity) {
 		float reach = FloatArgumentType.getFloat(context, "reach");
-		context.getSource().getPlayer().networkHandler.sendChatCommand("attribute @s minecraft:entity_interaction_range base set " + reach);
-		context.getSource().getPlayer().networkHandler.sendChatCommand("attribute @s minecraft:block_interaction_range base set " + reach);
+		ClientPlayNetworkHandler networkHandler = context.getSource().getPlayer().networkHandler;
+		if (block) networkHandler.sendChatCommand("attribute @s minecraft:block_interaction_range base set " + reach);
+		if (entity) networkHandler.sendChatCommand("attribute @s minecraft:entity_interaction_range base set " + reach);
+		return 0;
+	}
+
+	private int reset(CommandContext<FabricClientCommandSource> context, boolean block, boolean entity) {
+		ClientPlayNetworkHandler networkHandler = context.getSource().getPlayer().networkHandler;
+		if (block) networkHandler.sendChatCommand("attribute @s minecraft:block_interaction_range base reset");
+		if (entity) networkHandler.sendChatCommand("attribute @s minecraft:entity_interaction_range base reset");
 		return 0;
 	}
 }
